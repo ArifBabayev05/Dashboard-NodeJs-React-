b88a5fb882ffcef31f8fff35538c4a9221c2e1b4
Arif Babayev <arifrb@code.edu.az>
2022-10-31 04:27:34 +0400

    Notes finish

--- dashboard-/src/pages/CustomersDetail.jsx
+++ dashboard-/src/pages/CustomersDetail.jsx
@@ -7,7 +7,6 @@ import { useDispatch } from 'react-redux'
 
 //Company Details
 function CustomersDetail() {
-
     const { id } = useParams()
     const url = `http://localhost:2006/company/${id}`
     const [product, setProduct] = useState({
@@ -15,6 +14,8 @@ function CustomersDetail() {
         data: null,
         error: false
     })
+
+
 
     let content = null
 
@@ -43,8 +44,10 @@ function CustomersDetail() {
                     })
             })
     }, [url])
+    
 
-    const [Tdata, setData] = useState([])
+    const [Tdata, setTData] = useState([])
+    const [NoteData, setNoteData] = useState([])
     const [dataLength, setDataLength] = useState()
 
     useEffect(() => {
@@ -61,9 +64,40 @@ function CustomersDetail() {
 
             .then(res => {
 
-                setData(res.data)
+                setTData(res.data)
             }).catch(err => console.log(err))
     }, [])
+    useEffect(() => {
+        axios.get('http://localhost:2006/notes')
+
+            .then(res => {
+                setNoteData(res.data)
+            }).catch(err => console.log(err))
+    }, [])
+    const noteUrl = 'http://localhost:2006/notes';
+    const [data, setData] = useState({
+        text: "",
+        author: "",
+
+    })
+    function submit(e) {
+        e.preventDefault();
+
+        axios.post(noteUrl, {
+            text: data.text,
+            author: product.data.name
+        }).then(res => {
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+    function handle(e) {
+        const newData = { ...data }
+        newData[e.target.id] = e.target.value;
+        setData(newData);
+    }
+
+
 
     const array = Tdata.map((Tdata, index) => {
         return (
@@ -79,15 +113,15 @@ function CustomersDetail() {
                     {/* {array} */}
 
                     <h2 class="font-semibold text-xl">{Tdata.title}</h2>
-                    <h3 class="text-sm">{Tdata.createAt.slice(0,10)}</h3>
+                    <h3 class="text-sm">{Tdata.createAt.slice(0, 10)}</h3>
                     <h3 class="text-sm"><span class="font-semibold">Organization</span> : {Tdata.organisation}</h3>
                     <h3 class="text-sm"><span class="font-semibold">Value</span> : ${Tdata.valuet}</h3>
-                    <hr className='w-100' style={{"position":'absolute'}}/>
-                    <h3 class="text-sm"><span class="font-semibold text-slate-800">Contact</span></h3>
+                    <hr className='w-100' style={{ "position": 'absolute' }} />
+                    <h3 class="text-sm"><span class="font-semibold text-slate-800">Contact:</span></h3>
                     <h3 class="text-sm"><i class=" mr-2 fa-solid fa-phone"></i>{Tdata.tel}</h3>
                     <h3 class="text-sm"><i class=" mr-2 fa-solid fa-envelope"></i>{Tdata.mail}</h3>
                 </div>
-                
+
                 <div class="flex h-8 w-8 shrink-0 items-center justify-center self-center rounded-lg bg-orange-300 ring-orange-300 ring-offset-2 transition-colors duration-300 hover:bg-orange-400 focus:outline-none focus:ring-2" role="button" aria-label="Call  My wife " tabindex="1">
                     <i class="fa-sharp fa-solid fa-pen text-slate-800"></i>
                 </div>
@@ -96,9 +130,24 @@ function CustomersDetail() {
                 </div>
 
             </div>
-            
+
         )
     })
+    const notes = NoteData.map((NoteData, index) => {
+        return (
+            <div className='flex'>
+
+                <img class="w-26 h-16  mt-3 mr-2 rounded-full" src="https://play-lh.googleusercontent.com/JOPJ9dbtmH9FZKNweSkJN89I6Yy7dYJ_8kQB9eHYN_PPpH-p0gas76UDhXPes3OSVqQ" alt="image description" />
+                <a href="#" class="w-100 block p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" style={{ 'width': '100%' }}>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Author: {NoteData.author}</h5>
+                    <p class="font-normal text-gray-700 dark:text-gray-400">Message: {NoteData.text}</p>
+                    <p class="font-normal text-gray-400 dark:text-gray-400">{NoteData.createAt.slice(0,10)}</p>
+
+                </a>
+            </div>
+
+        )
+    }).reverse();
 
 
 
@@ -107,7 +156,7 @@ function CustomersDetail() {
     }
 
     if (product.error) {
-        content = <p>Xəta baş verdi, yenidən yoxlayın.</p>
+        content = <p>Error! Try Again!</p>
     }
     if (product.data) {
         content =
@@ -134,31 +183,54 @@ function CustomersDetail() {
 
                     </div>
 
+                    {/* Sidebar */}
                     {array}
                 </div>
-                <div class="flex flex-1 flex-col justify-start gap-3 bg-slate-50 p-3">
-                    <div class="max-w-fit rounded-tr-lg rounded-bl-lg bg-blue-500 text-white px-2 py-1 shadow-sm">hi have you visited Maka ?</div>
-                    <div class="max-w-fit self-end rounded-tl-lg rounded-br-lg bg-white px-2 py-1">Yes I enjoy my time there</div>
-                    <div class="max-w-fit rounded-tr-lg rounded-bl-lg bg-blue-500 text-white  px-2 py-1 ">Do you take some pictures ?</div>
-                    <div class="max-w-fit self-end rounded-tl-lg rounded-br-lg bg-white px-2 py-1">Yes I do</div>
-                    <div class="max-w-fit rounded-tr-lg rounded-bl-lg bg-blue-500 text-white  px-2 py-1">Please Send me some</div>
 
-                    <div class="flex max-w-xs flex-wrap justify-end gap-2 self-end">
-                        <div class="h-20 w-28 overflow-hidden rounded border-2  bg-slate-400">
-                            <img class="h-20 w-28 object-cover object-center" src="https://images.unsplash.com/photo-1565552645632-d725f8bfc19a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" alt="Maka" />
-                        </div>
-                        <div class="h-20 w-28 overflow-hidden rounded border-2 bg-slate-400">
-                            <img class="h-20 w-28 object-cover object-center" src="https://images.unsplash.com/photo-1580109158791-5cfcb80419e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80" alt="Maka" />
-                        </div>
-                        <div class="relative h-20 w-28 overflow-hidden rounded border-2 bg-slate-400">
-                            <div class="absolute inset-0 flex items-center justify-center bg-slate-900/40">
-                                <span class="font-semibold text-white">+2</span>
-                            </div>
-                            <img class="h-20 w-28 object-cover object-center" src="https://images.unsplash.com/photo-1591604176169-ed3393581c4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Maka" />
-                        </div>
+                {/* Posts */}
+                <div class="flex flex-1 flex-col justify-start gap-3 bg-slate-50 p-3">
+                    <div>
+                        <button type="button" class="w-25 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-sm text-sm px-3 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            <i class="fa-solid fa-message mr-2"></i>
+                            Note
+                        </button>
+                        <button type="button" class="w-25 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-sm text-sm px-3 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            <i class="fa-solid fa-envelope mr-2"></i>
+                            Email
+                        </button>
+
+                        <button type="button" class="w-25 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-sm text-sm px-3 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            <i class="fa-solid fa-comments mr-2"></i>
+                            Sms
+                        </button>
+                        <button type="button" class="w-25 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-sm text-sm px-3 py-1.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                            <i class="fa-solid fa-phone mr-2"></i>
+                            Call
+                            <i class=" ml-2 fa-solid fa-caret-down"></i>
+                        </button>
                     </div>
-                    <div class="max-w-fit rounded-tr-lg rounded-bl-lg bg-blue-500 text-white  px-2 py-1">جزاك الله خيرا</div>
+
+                    <form enctype="multipart/form-data" method="post" onSubmit={(e) => submit(e)}>
+                        <div class="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+                            <div class="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
+                                <label for="comment" class="sr-only">Your comment</label>
+                                <textarea onChange={(e) => handle(e)} value={data.value} required type="text" id="text" rows="4" class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a note..."></textarea>
+                            </div>
+                            <div class="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
+                                <div></div>
+                                <div class="flex pl-0 space-x-1 sm:pl-2">
+                                    <button type="submit" class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-gray-700 rounded-lg focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-900 hover:bg-gray-800">
+                                        Done
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                    {notes}
+
                 </div>
+
             </div>
 
 
